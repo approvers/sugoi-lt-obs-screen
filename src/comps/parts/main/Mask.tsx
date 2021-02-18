810dbc2db2c05b5a37ed9f703f3e2b58566b64bc
loxygenk <loxygen.k@gmail.com>
2021-02-18 21:13:09 +0900

    🐛 Hide overflow when too much cards are shown

--- src/comps/parts/main/Mask.tsx
+++ src/comps/parts/main/Mask.tsx
@@ -6,13 +6,15 @@ export const Mask = () => {
   const {width} = useWindowDimensions();
 
   // TODO: この数値をどうにかしたい
-  const netWidth = width * 0.808;
+  const netWidth = width * 0.8075;
 
   return (
       <div
         className={styles.mask}
         style={{
           width: `${netWidth}px`,
+          minWidth: `${netWidth}px`,
+          maxWidth: `${netWidth}px`,
           height: `${netWidth * (9 / 16)}px`
         }}
       />
--- src/comps/parts/main/Timeline.tsx
+++ src/comps/parts/main/Timeline.tsx
@@ -1,29 +1,37 @@
 import * as React from "react";
 import styles from "../../../style/comps/main.module.scss";
 import {TimelineCard} from "../../../lib/data/ScreenData";
+import {useWindowDimensions} from "../../../lib/WindowHooks";
 
 type TimelineProps = {
   timeline: TimelineCard[]
 };
-export const Timeline: React.FC<TimelineProps> = ({timeline}) => (
-  <div className={styles.timeline}>
-    {
-      timeline.map(
-        (card, index) => (
-          <div className={styles.card} key={index}>
-            <p className={styles.card_content}>{card.content}</p>
-            <div className={styles.card_detail}>
-              <span>
-                {card.user.name}
-                (<span className={styles.card_ident}>@{card.user.identifier}</span>)
-              </span>
-              <span>
-                {card.service.charAt(0)}
-              </span>
+export const Timeline: React.FC<TimelineProps> = ({timeline}) => {
+  const {width} = useWindowDimensions();
+
+  // TODO: この数値をどうにかしたい
+  const netWidth = width * 0.8075;
+
+  return (
+    <div className={styles.timeline} style={{height: `${netWidth * (9 / 16)}px`}}>
+      {
+        timeline.map(
+          (card, index) => (
+            <div className={styles.card} key={index}>
+              <p className={styles.card_content}>{card.content}</p>
+              <div className={styles.card_detail}>
+                <span>
+                  {card.user.name}
+                  (<span className={styles.card_ident}>@{card.user.identifier}</span>)
+                </span>
+                <span>
+                  {card.service.charAt(0)}
+                </span>
+              </div>
             </div>
-          </div>
+          )
         )
-      )
-    }
-  </div>
-)
+      }
+    </div>
+  );
+}
