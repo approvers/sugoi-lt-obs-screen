c9b441c0e6cbde9bebc0d2f1770f934d0b0341e2
kawaemon <34652535+kawaemon@users.noreply.github.com>
2021-02-19 12:20:44 +0900

    feat: interval test event sending

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,26 +3,38 @@
     windows_subsystem = "windows"
 )]
 
+use std::time::Duration;
+use tauri::{Webview, WebviewMut};
+use tokio::runtime::{Builder as TokioRuntimeBuilder, Runtime as TokioRuntime};
+
 mod cmd;
 
 fn main() {
+    let mut rt = TokioRuntimeBuilder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
     tauri::AppBuilder::new()
-        .invoke_handler(|_webview, arg| {
-            use cmd::Cmd::*;
-            match serde_json::from_str(arg) {
-                Err(e) => Err(e.to_string()),
-                Ok(command) => {
-                    match command {
-                        // definitions for your custom commands from Cmd here
-                        MyCustomCommand { argument } => {
-                            //  your command code
-                            println!("{}", argument);
-                        }
-                    }
-                    Ok(())
-                }
-            }
-        })
+        .setup(move |a, b| setup(&mut rt, a, b))
+        .invoke_handler(on_client_message)
         .build()
         .run();
+}
+
+fn setup(rt: &mut TokioRuntime, webview: &mut Webview, _source: String) {
+    let mut webview: WebviewMut = webview.as_mut();
+
+    rt.spawn(async move {
+        let mut interval = tokio::time::interval(Duration::from_secs(5));
+        loop {
+            interval.tick().await;
+            tauri::event::emit(&mut webview, "test", Some("hogehoge")).unwrap();
+            println!("emit!");
+        }
+    });
+}
+
+fn on_client_message(_webview: &mut Webview, _message: &str) -> Result<(), String> {
+    Ok(())
 }
