e01a028698895e5ba9935c218487c878be1e85ed
loxygenk <loxygen.k@gmail.com>
2021-02-23 02:33:18 +0900

    ✨ Add subtitle

--- src/comps/waiting/parts/Title.tsx
+++ src/comps/waiting/parts/Title.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import styles from "../../../style/waiting/waiting_screen.module.scss";
 
 export const Title = () => (
-  <div className={styles.title}>
+  <div className={styles.title + " " + styles.title_border}>
     <img className={styles.title_logo} src={"/approvers_icon.png"} />
-    <p className={styles.title_subtitle}>限界開発鯖 1周年記念LT企画</p>
+    <p className={styles.title_caption}>限界開発鯖 1周年記念LT企画</p>
     <p className={styles.title_title}>第2回 限界LT</p>
+    <p className={styles.title_subtitle}>First Anniversary</p>
     <p className={styles.title_message}>開始までしばらくおまちください</p>
   </div>
 )
