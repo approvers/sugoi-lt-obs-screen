99c9f0cb40ab1e8bcd063ff6d47ea2d624532748
loxygenk <loxygen.k@gmail.com>
2021-02-23 03:31:56 +0900

    ✨ Created pending presentations list

--- src/comps/waiting/WaitingScreen.tsx
+++ src/comps/waiting/WaitingScreen.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import styles from "../../style/waiting/waiting_screen.module.scss";
 import { Title } from "./parts/Title";
+import {PresenterList} from "./parts/PresenterList";
 import { ScreenData } from "../../lib/data/ScreenData";
 
 type WaitingScreenProps = {
@@ -10,5 +11,6 @@ export const WaitingScreen: React.FC<WaitingScreenProps> = ({state}) => (
   <div className={styles.wrapper}>
     <img className={styles.background} src={"/evil_spirits.png"} />
     <Title message={state.notification ?? "しばらくおまちください"}/>
+    <PresenterList presentations={state.pending_presentation}/>
   </div>
 )
